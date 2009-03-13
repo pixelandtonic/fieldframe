41984fa5b1e9345a5a9eeac98d51e83f45c76adf
Brandon Kelly <me@brandon-kelly.com>
2009-03-12 23:31:45 -0700

    ff_matrix files

---
+++ extensions/fieldtypes/ff_matrix/ft.ff_matrix.php
@@ -0,0 +1,44 @@
+<?php
+
+if ( ! defined('EXT')) exit('Invalid file request');
+
+
+/**
+ * FF Matrix Class
+ *
+ * @package   FieldFrame
+ * @author    Brandon Kelly <me@brandon-kelly.com>
+ * @copyright Copyright (c) 2009 Brandon Kelly
+ * @license   http://creativecommons.org/licenses/by-sa/3.0/ Attribution-Share Alike 3.0 Unported
+ */
+class Ff_matrix extends Fieldframe_Fieldtype {
+
+	/**
+	 * Fieldtype Info
+	 * @var array
+	 */
+	var $info = array(
+		'name'     => 'FF Matrix',
+		'version'  => '0.0.1',
+		'desc'     => 'Provides a tabular data fieldtype',
+		'docs_url' => 'http://wiki.github.com/brandonkelly/bk.fieldframe.ee_addon/ff-checkbox'
+	);
+
+	/**
+	 * Display Field
+	 * 
+	 * @param  string  $field_name      The field's name
+	 * @param  mixed   $field_data      The field's current value
+	 * @param  array   $field_settings  The field's settings
+	 * @return string  The field's HTML
+	 */
+	function display_field($field_name, $field_data, $field_settings)
+	{
+		
+	}
+
+}
+
+
+/* End of file ft.ff_matrix.php */
+/* Location: ./system/fieldtypes/ff_matrix/ft.ff_matrix.php */
---
+++ language/english/lang.ff_matrix.php
@@ -0,0 +1,10 @@
+<?php
+
+$L = array(
+
+
+''=>''
+);
+
+/* End of file lang.ff_matrix.php */
+/* Location: ./system/language/english/lang.ff_matrix.php */
